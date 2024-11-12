81bdce159b2986c89152a2ca28a9b8a3181ea3ab
Harrison <harrisonaffel@gmail.com>
2024-11-12 16:13:29 -0500

    [2.9] October Patches (#1550)
    release october patches from dev-v2.9

--- pkg/rke/templates/templates.go
+++ pkg/rke/templates/templates.go
@@ -131,6 +131,7 @@ const (
 	nginxIngressv1101Rancher1 = "nginxingress-v1.10.1-rancher1"
 	nginxIngressv1112Rancher1 = "nginxingress-v1.11.2-rancher1"
 	nginxIngressv1112Rancher2 = "nginxingress-v1.11.2-rancher2"
+	nginxIngressv1113Rancher1 = "nginxingress-v1.11.3-rancher1"
 
 	nodelocalv115 = "nodelocal-v1.15"
 	nodelocalv121 = "nodelocal-v1.21"
@@ -360,13 +361,16 @@ func LoadK8sVersionedTemplates() map[string]map[string]string {
 			">=1.27.8-rancher2-1 <1.28.6-rancher1-1":   nginxIngressv194Rancher1,
 			">=1.28.6-rancher1-1 <1.28.13-rancher1-1":  nginxIngressv196Rancher1,
 			">=1.28.13-rancher1-1 <1.28.14-rancher1-1": nginxIngressv1112Rancher1,
-			">=1.28.14-rancher1-1 <1.29.0-rancher1-1":  nginxIngressv1112Rancher2,
+			">=1.28.14-rancher1-1 <1.28.15-rancher1-1": nginxIngressv1112Rancher2,
+			">=1.28.15-rancher1-1 <1.29.0-rancher1-1":  nginxIngressv1113Rancher1,
 			">=1.29.0-rancher1-1 <1.29.8-rancher1-1":   nginxIngressv196Rancher1,
 			">=1.29.8-rancher1-1 <1.29.9-rancher1-1":   nginxIngressv1112Rancher1,
-			">=1.29.9-rancher1-1 <1.30.0-rancher1-1":   nginxIngressv1112Rancher2,
+			">=1.29.9-rancher1-1 <1.29.10-rancher1-1":  nginxIngressv1112Rancher2,
+			">=1.29.10-rancher1-1 <1.30.0-rancher1-1":  nginxIngressv1113Rancher1,
 			">=1.30.0-rancher1-1 <1.30.4-rancher1-1":   nginxIngressv1101Rancher1,
 			">=1.30.4-rancher1-1 <1.30.5-rancher1-1":   nginxIngressv1112Rancher1,
-			">=1.30.5-rancher1-1":                      nginxIngressv1112Rancher2,
+			">=1.30.5-rancher1-1 <1.30.6-rancher1-1":   nginxIngressv1112Rancher2,
+			">=1.30.6-rancher1-1":                      nginxIngressv1113Rancher1,
 		},
 		kdm.Nodelocal: {
 			">=1.15.11-rancher0 <1.16.0-alpha":     nodelocalv115,
@@ -497,6 +501,7 @@ func getTemplates() map[string]string {
 		nginxIngressv1101Rancher1: NginxIngressTemplateV1101Rancher1,
 		nginxIngressv1112Rancher1: NginxIngressTemplateV1112Rancher1,
 		nginxIngressv1112Rancher2: NginxIngressTemplateV1112Rancher2,
+		nginxIngressv1113Rancher1: NginxIngressTemplateV1113Rancher1,
 
 		nodelocalv115: NodelocalTemplateV115,
 		nodelocalv121: NodelocalTemplateV121,
